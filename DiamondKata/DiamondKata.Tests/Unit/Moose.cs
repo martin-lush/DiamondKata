9e3a32455f95cf9c42cd42f9b2a0a4b5b4df1653
Martin Lush <martin-hornsby@ntlworld.com>
2022-08-07 17:16:43 +0100

    Found more 'Moose' tests in the tutorial

--- DiamondKata/DiamondKata.Tests/Unit/Moose.cs
+++ DiamondKata/DiamondKata.Tests/Unit/Moose.cs
@@ -43,5 +43,36 @@ namespace DiamondKata.Tests.Unit
             var result = new Diamond().PrintDiamond('B').RemoveNewLines();
             Assert.AreEqual($" A B B A ", result);
         }
+
+
+        [Test]
+        public void B_Should_B_Bigger_Than_A()
+        {
+            var sut = new Diamond();
+            var a = sut.PrintDiamond('A');
+            var b = sut.PrintDiamond('B');
+
+            Assert.Greater(b.Length, a.Length);
+        }
+
+        [Test]
+        public void C_should_be_Bigger_Than_B()
+        {
+            var sut = new Diamond();
+            var b = sut.PrintDiamond('B');
+            var c = sut.PrintDiamond('C');
+
+            Assert.Greater(c.Length, b.Length);
+        }
+
+        [Test]
+        public void D_should_be_Bigger_Than_C()
+        {
+            var sut = new Diamond();
+            var c = sut.PrintDiamond('C');
+            var d = sut.PrintDiamond('D');
+
+            Assert.Greater(d.Length, c.Length);
+        }
     }
 }
